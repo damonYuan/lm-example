6764202453d1d7b6d3c37773b3070c1d104584ce
damon.yuan <damon.yuan@okg.com>
2024-03-27 18:33:19 +0800

    udpate

---
+++ README.md
@@ -0,0 +1,12 @@
+README
+====
+
+Example for [OLS with Levenberg-Marquardt Optimizer](https://commons.apache.org/proper/commons-math/userguide/leastsquares.html).
+
+# TODO
+
+1. The first one is based on the Gauss-Newton method. 
+2. The second one is the Levenberg-Marquardt method.
+3. .lazyEvaluation(false) -> what is this
+4. .maxEvaluations(1000)  -> what is this
+5. .maxIterations(1000) -> what is this
--- src/main/java/com/damonyuan/test/App.java
+++ src/main/java/com/damonyuan/test/App.java
@@ -1,82 +1,75 @@
 package com.damonyuan.test;
 
-import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
-import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem;
-import org.apache.commons.math3.fitting.leastsquares.MultivariateJacobianFunction;
-import org.apache.commons.math3.fitting.leastsquares.LeastSquaresBuilder;
-import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
-import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.fitting.leastsquares.*;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.util.Pair;
 
-/**
- * Hello world!
- */
+import static java.lang.Math.sqrt;
+import static java.util.Arrays.fill;
+
 public class App {
+    // The following simple example shows how to find the center of a circle of known radius to best fit observed 2D points.
     public static void main(String[] args) {
-        // Define the observed data (target values)
-        // y = x^2 + x + 1
-        final double[] xData = {1, 2, 3, 4, 5, -1};
-        final double[] yData = {3.01, 6.99, 13.01, 20.99, 30.99, 1};
+        // (x-a)^2 + (y-b)^2 = r^2
+        // f(a, b) = sqr((x-a)^2 + (y-b)^2) - r
+        final double radius = 1;
+        final double[] xs = new double[]{0, 1, 2, 1};
+        final double[] ys = new double[]{1, 2, 1, 0};
+        // obviously the fitting result should be close to (1, 1)
+        final int numOfPoints = xs.length;
 
-        // Define the initial guess for the parameters
-        double[] initialGuess = {1.0, 1.0, 1.0};
-
-        // Create the objective function implementing MultivariateJacobianFunction
-        MultivariateJacobianFunction objectiveFunction = new MultivariateJacobianFunction() {
+        // the model function components are the distances to current estimated center,
+        // they should be as close as possible to the specified radius
+        final MultivariateJacobianFunction distancesToCurrentCenter = new MultivariateJacobianFunction() {
             @Override
             public Pair<RealVector, RealMatrix> value(final RealVector point) {
-                double a = point.getEntry(0);
-                double b = point.getEntry(1);
-                double c = point.getEntry(2);
 
-                int numDataPoints = xData.length;
+                final double a = point.getEntry(0);
+                final double b = point.getEntry(1);
 
-                // Compute the residuals (errors) and Jacobian matrix
-                double[] residuals = new double[numDataPoints];
-                double[][] jacobianData = new double[numDataPoints][3];
+                final RealVector value = new ArrayRealVector(numOfPoints);
+                final RealMatrix jacobian = new Array2DRowRealMatrix(numOfPoints, 2);
 
-                for (int i = 0; i < numDataPoints; i++) {
-                    double x = xData[i];
-                    double y = yData[i];
-
-                    residuals[i] = y - (a * x * x + b * x + c);
-
-                    // Compute the partial derivatives (Jacobian matrix)
-                    jacobianData[i][0] = -x * x;
-                    jacobianData[i][1] = -x;
-                    jacobianData[i][2] = -1.0;
+                for (int i = 0; i < numOfPoints; ++i) {
+                    final double x = xs[i];
+                    final double y = ys[i];
+                    final double distance = sqrt((x - a) * (x - a) + (y - b) * (y - b));
+                    final double residual = distance - radius;
+                    value.setEntry(i, residual);
+                    // df/da = -(x-a)/sqrt((x-a)^2 + (y-b)^2)
+                    jacobian.setEntry(i, 0, -(x - a) / distance);
+                    // df/db = -(y-b)/sqrt((x-a)^2 + (y-b)^2)
+                    jacobian.setEntry(i, 1, -(y - b) / distance);
                 }
 
-                RealVector residualsVector = MatrixUtils.createRealVector(residuals);
-                RealMatrix jacobianMatrix = MatrixUtils.createRealMatrix(jacobianData);
-
-                return new Pair<>(residualsVector, jacobianMatrix);
+                return new Pair<>(value, jacobian);
             }
         };
 
-        // Create the Levenberg-Marquardt optimizer
-        LevenbergMarquardtOptimizer optimizer = new LevenbergMarquardtOptimizer();
+        // the target is f(a, b) = sqr((x-a)^2 + (y-b)^2) - r = 0
+        final double[] prescribedDistances = new double[numOfPoints];
+        fill(prescribedDistances, 0);
 
-        // Build the least squares problem
-        LeastSquaresProblem problem = new LeastSquaresBuilder()
-                .model(objectiveFunction)
-                .target(yData)
+        // least squares problem to solve : modeled radius should be close to target radius
+        final LeastSquaresProblem problem = new LeastSquaresBuilder()
+                .start(new double[]{100, 50})
+                .model(distancesToCurrentCenter)
+                .target(prescribedDistances)
                 .lazyEvaluation(false)
                 .maxEvaluations(1000)
                 .maxIterations(1000)
-                .start(initialGuess)
                 .build();
+        System.out.println("Observation size: " + problem.getObservationSize());
+        System.out.println("Parameter size: " + problem.getParameterSize());
+        System.out.println("Start with: " + problem.getStart());
 
-        // Run the optimization
-        final LeastSquaresOptimizer.Optimum optimum = optimizer.optimize(problem);
-        RealVector optimizedParameters = optimum.getPoint();
-
-        // Print the optimized parameters
-        System.out.println("Optimized parameters:");
-        System.out.println("a = " + optimizedParameters.getEntry(0));
-        System.out.println("b = " + optimizedParameters.getEntry(1));
-        System.out.println("c = " + optimizedParameters.getEntry(2));
+        final LeastSquaresOptimizer.Optimum optimum = new LevenbergMarquardtOptimizer().optimize(problem);
+        System.out.println("fitted center: " + optimum.getPoint().getEntry(0) + " " + optimum.getPoint().getEntry(1));
+        System.out.println("RMS: " + optimum.getRMS());
+        System.out.println("evaluations: " + optimum.getEvaluations());
+        System.out.println("iterations: " + optimum.getIterations());
     }
 }
